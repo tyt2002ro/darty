77fa64c8d97d103dec072aee7d710daab9ecc27f
DESKTOP-MV19CH7\DEV-31 <marius.sandulache@axonsoft.ro>
2022-10-06 10:49:35 +0300

    delete player

---
+++ symfony/my_project_directory/src/Controller/DeletePlayerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Player;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+
+class DeletePlayerController extends AbstractController
+{
+    #[Route('/delete/player/{id}', name: 'delete_player')]
+    public function delete(ManagerRegistry $doctrine, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $player = $entityManager->getRepository(Player::class)->find($id);
+
+        if (!$player) {
+            throw $this->createNotFoundException(
+                'No player found for id '.$id
+            );
+        }
+
+        $entityManager->remove($player);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('playerManagement');
+    }
+}
