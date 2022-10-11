d4f40c45060af93738980f0b85bbc6e5370531e8
Ionica Alexandru Pintea <i.pintea@neusta.de>
2022-10-11 14:04:07 +0300

    added test editPlayerActionFromRequest

--- symfony/my_project_directory/tests/Unit/Controller/EditPlayerControllerTest.php
+++ symfony/my_project_directory/tests/Unit/Controller/EditPlayerControllerTest.php
@@ -1,7 +1,9 @@
 <?php
+
 use App\Controller\EditPlayerController;
 use App\Entity\Player;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ObjectManager;
 use Doctrine\Persistence\ObjectRepository;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -9,6 +11,7 @@ use Symfony\Component\Form\FormFactory;
 use Symfony\Component\Form\FormInterface;
 use Prophecy\Argument;
 use Symfony\Component\Form\FormView;
+use Symfony\Component\HttpFoundation\Request;
 use Twig\Environment;
 
 final class EditPlayerControllerTest extends TestCase
@@ -52,7 +55,49 @@ final class EditPlayerControllerTest extends TestCase
 
         $twig->render(Argument::cetera())->shouldBeCalled()->willReturn($content);
 
-        $result = $editPlayerController->editPlayerFormAction($managerRegistryMock->reveal(),$player->getId());
-        self::assertSame($content,$result->getContent());
+        $result = $editPlayerController->editPlayerFormAction($managerRegistryMock->reveal(), $player->getId());
+        self::assertSame($content, $result->getContent());
+    }
+
+    /**
+     * @test
+     * @throws \App\Exceptions\PlayerNotExistException
+     */
+    public function editPlayerActionFromRequest(): void
+    {
+        $expectedUrl = '/player-management';
+        $playerId = 2;
+        $player = new Player();
+        $player->setId($playerId);
+
+        $editPlayerController = new EditPlayerController();
+        $request = new Request();
+
+        $managerRegistry = $this->prophesize(ManagerRegistry::class);
+        $objectRepository = $this->prophesize(ObjectRepository::class);
+        $container = $this->prophesize(ContainerInterface::class);
+        $formFactory = $this->prophesize(FormFactory::class);
+        $formInterface = $this->prophesize(FormInterface::class);
+        $objectManager = $this->prophesize(ObjectManager::class);
+        $playerMock = $this->prophesize(Player::class);
+
+        $managerRegistry->getRepository(Player::class)->shouldBeCalled()->willReturn($objectRepository->reveal());
+        $objectRepository->find($player->getId())->shouldBeCalled()->willReturn($player);
+        $editPlayerController->setContainer($container->reveal());
+        $container->get('form.factory')->shouldBeCalled()->willReturn($formFactory->reveal());
+        $formFactory->create(Argument::cetera())->shouldBeCalled()->willReturn($formInterface->reveal());
+
+        $formInterface->handleRequest($request)->shouldBeCalled()->willReturn($formInterface->reveal());
+        $formInterface->isSubmitted()->shouldBeCalled()->willReturn(true);
+        $formInterface->isValid()->shouldBeCalled()->willReturn(true);
+        $formInterface->getData()->shouldBeCalled()->willReturn($playerMock->reveal());
+
+        $managerRegistry->getManager()->shouldBeCalled()->willReturn($objectManager->reveal());
+
+        $objectManager->persist($playerMock->reveal())->shouldBeCalled();
+        $objectManager->flush()->shouldBeCalled();
+
+        $result = $editPlayerController->editPlayerAction($managerRegistry->reveal(), $request, $playerId);
+        self::assertSame($expectedUrl, $result->getTargetUrl());
     }
 }
