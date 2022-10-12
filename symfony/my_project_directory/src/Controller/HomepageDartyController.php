2bf6d862ce41fc6413982eb0c594c9cfcf141d13
ax <tudor.abrudan@axonsoft.ro>
2022-10-12 11:11:44 +0300

    fix import class

--- symfony/my_project_directory/src/Controller/HomepageDartyController.php
+++ symfony/my_project_directory/src/Controller/HomepageDartyController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Entity\Player;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
